799f67bb56897dc96535720e17cdd7200667902a
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2024-05-19 04:20:24 +0000

    Sync LeetCode submission Runtime - 42 ms (5.18%), Memory - 16.7 MB (100.00%)

---
+++ solutions/2292-counting-words-with-a-given-prefix/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        c=0
+        for word in words:
+            if(word[:len(pref)]==pref):
+                c+=1
+        return c        
+        
