b101711d9afe9f3cc3bc0f0cb19bb28661f485c0
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2024-09-20 12:45:44 +0000

    Sync LeetCode submission Runtime - 57 ms (5.90%), Memory - 16.7 MB (100.00%)

---
+++ solutions/0009-palindrome-number/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+        rev=0
+        temp=x
+        while(x>0):
+            rev=rev*10+x%10
+            x//=10
+        if rev==temp:
+            return True
+        return False
+
+        
