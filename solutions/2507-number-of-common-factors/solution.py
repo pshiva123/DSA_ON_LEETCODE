20b9df6c6bd14bfe55e67125bf66d5069901cbcc
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2024-10-24 06:43:40 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 16.8 MB (100.00%)

---
+++ solutions/2507-number-of-common-factors/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def commonFactors(self, a: int, b: int) -> int:
+        k=min(a,b)
+        c=0
+        for i in range(1,k+1):
+            if a%i==0 and b%i==0:
+                c+=1
+        return c
+        
