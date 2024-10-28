bdc7db977e122697a2368ce7898ff3216b6a72c7
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2024-10-28 10:35:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 16.7 MB (100.00%)

--- solutions/0075-sort-colors/solution.py
+++ solutions/0075-sort-colors/solution.py
@@ -1,24 +1,21 @@
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+    def sortColors(self, arr: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zero=0
-        one=0
-        two=0
-        for i in nums:
-            if i==1:
-                one+=1
-            elif i==2:
-                two+=1
-            else:
-                zero+=1
-        for i in range(zero):
-            nums[i]=0
-        for i in range(zero,zero+one):
-            nums[i]=1
-        for i in range(zero+one,zero+one+two):
-            nums[i]=2
-            
+        low=0
+        high=len(arr)-1
+        mid=0
+        while(mid<=high):
 
+            if arr[mid]==0:
+                arr[low],arr[mid]=arr[mid],arr[low]
+                low+=1
+                mid+=1
+            elif arr[mid]==1:
+                mid+=1
+            else:
+                arr[high],arr[mid]=arr[mid],arr[high]
+                high-=1
+        
         
