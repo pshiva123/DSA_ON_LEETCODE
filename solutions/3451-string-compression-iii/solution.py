c88e0f4912aea98ae0d5b4829a196bbadc245c16
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2024-11-04 16:47:47 +0000

    Sync LeetCode submission Runtime - 223 ms (35.67%), Memory - 19 MB (100.00%)

--- solutions/3451-string-compression-iii/solution.py
+++ solutions/3451-string-compression-iii/solution.py
@@ -1,33 +1,22 @@
 class Solution:
     def compressedString(self, word: str) -> str:
-        if(len(word)==1):
-            return "1"+word[0]
-        elif(len(word)==0):
-            return "0"
-        else:
-            st=""
-            c=1
-            for i in range(0,len(word)-1):
-                if(c==9):
-                    k=str(c)
-                    st=st+k
-                    st=st+word[i]
-                    c=1
-                    continue
-                if(word[i]==word[i+1]):
-                    c+=1
-                else:
-                    k=str(c)
-                    st=st+k
-                    st=st+word[i]
-                    c=1
-            if(word[-1]==word[-2]):
-                k=str(c)
-                st=st+k
-                st=st+word[-1]
+        s=""
+        c=1
+        for i in range(1,len(word)):
+            if word[i]==word[i-1]:
+                if c==9:
+                    s+=str(c)
+                    s+=word[i-1]
+                    c=0
+                c+=1
             else:
-                st=st+"1"
-                st=st+word[-1]
-        return st
-            
+                s+=str(c)
+                s+=word[i-1]
+                c=1
+        s+=str(c)
+        s+=word[-1]
+        return s
+        
+
+
         
