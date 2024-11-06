f784b68668b9b3b24e2ce4e2650aed5ef7d62dc5
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2024-11-06 02:25:34 +0000

    Sync LeetCode submission Runtime - 4 ms (76.04%), Memory - 17.4 MB (100.00%)

--- solutions/0073-set-matrix-zeroes/solution.py
+++ solutions/0073-set-matrix-zeroes/solution.py
@@ -3,19 +3,28 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rows=set()
-        cols=set()
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
+        clo=1
+        m=len(matrix)
+        n=len(matrix[0])
+        for i in range(0,m):
+            for j in range(0,n):
                 if matrix[i][j]==0:
-                    rows.add(i)
-                    cols.add(j)
-        rows=list(rows)
-        cols=list(cols)
-        for i in rows:
-            for j in range(len(matrix[0])):
-                matrix[i][j]=0
-        for i in cols:
-            for j in  range(len(matrix)):
-                matrix[j][i]=0                    
+                    if j==0:
+                        clo=0
+                        matrix[i][0]=0
+                    else:
+                        matrix[0][j]=0
+                        matrix[i][0]=0
+        for i in range(m-1,0,-1):
+            for j in range(n-1,0,-1):
+                if matrix[i][0]==0 or matrix[0][j]==0:
+                    matrix[i][j]=0
+        for j in range(1,n):
+            if matrix[0][0]==0:
+                matrix[0][j]=0
+        for i in range(m):
+            if clo==0:
+                matrix[i][0]=0
+
+
         
