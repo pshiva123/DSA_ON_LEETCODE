aa69d94ac7beae1e6549d271e15e4049337797b2
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2024-11-27 06:56:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.2 MB (100.00%)

---
+++ solutions/0162-find-peak-element/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findPeakElement(self, arr: List[int]) -> int:
+        low=0
+        high=len(arr)-1
+        while(low<high):
+            mid=(low+high)//2
+            if arr[mid]<=arr[mid+1]:
+                low=mid+1
+            else:
+                high=mid
+        return low
+
+        
