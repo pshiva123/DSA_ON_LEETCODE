2cbe9c3f1aa56c8a55c786ce5d0f56b56a81813e
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2024-12-22 03:11:15 +0000

    Sync LeetCode submission Runtime - 7 ms (32.88%), Memory - 17.8 MB (100.00%)

---
+++ solutions/3656-minimum-number-of-operations-to-make-elements-in-array-distinct/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        c=0
+        while len(nums)!=len(set(nums)):
+            nums=nums[3:]
+            c+=1 
+        return c
+                
+            
+            
+        
