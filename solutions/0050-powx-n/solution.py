da2b65e3e059c3d886e8c9b96e4db16945583f0a
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2025-01-29 13:37:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

--- solutions/0050-powx-n/solution.py
+++ solutions/0050-powx-n/solution.py
@@ -1,4 +1,18 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return math.pow(x,n)
+        ans=1.0
+        temp=n
+        if n<0:
+            n=-1*n
+        while n>0:
+            if n%2==0:
+                n=n//2
+                x*=x
+            else:
+                ans=ans*x
+                n-=1
+        if temp<0:
+            return 1.0/ans
+        return ans
+
         
