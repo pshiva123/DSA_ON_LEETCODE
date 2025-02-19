bebb0d3099100322d7994a6b972e8054776cc5f2
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2025-02-19 13:38:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.1 MB (100.00%)

--- solutions/0078-subsets/solution.py
+++ solutions/0078-subsets/solution.py
@@ -1,17 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        def sub(nums,idx,n,li,temp):
+            if idx==n:
+                temp.append(li.copy())
+                return
+            li.append(nums[idx])
+            sub(nums,idx+1,n,li,temp)
+            li.pop()
+            sub(nums,idx+1,n,li,temp)
+        temp=[]
         li=[]
-        for i in range(2**len(nums)-1):
-            l2=[]
-            index=0
-            while(i>0):
-                if(i&1):
-                    l2.append(nums[index])
-                index+=1
-                i=i>>1
-            li.append(l2)
-        li.append(nums)
-        return li
-
+        sub(nums,0,len(nums),li,temp)
+        return temp
 
         
