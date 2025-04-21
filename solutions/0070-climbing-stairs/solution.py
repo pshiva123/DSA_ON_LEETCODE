f78af7c670391ee68c906e3e9bfd7702691660ca
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2025-04-21 04:37:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

---
+++ solutions/0070-climbing-stairs/solution.py
@@ -0,0 +1,12 @@
+
+class Solution:
+    def climbStairs(self, n):
+        dp=[0]*(n+1)
+        dp[1]=1
+        if n<=1:
+            return dp[n]
+        dp[2]=2
+        for i in range(3,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
+        
