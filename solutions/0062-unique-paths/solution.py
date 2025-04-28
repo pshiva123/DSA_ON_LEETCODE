2e6375bb9a4ee7c97c4dea0bb83e549d9e74cc6c
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2025-04-28 18:32:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (100.00%)

---
+++ solutions/0062-unique-paths/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def uniquePaths(self, n: int, m: int) -> int:
+        if m==1 and n==1:
+            return 1
+        dp=[[0 for j in range(m)] for i in range(n)]
+        for i in range(m):
+            dp[0][i]=1
+        for j in range(n):
+            dp[j][0]=1
+        dp[0][0]=0
+        for i in range(1,n):
+            for j in range(1,m):
+                dp[i][j]=dp[i-1][j]+dp[i][j-1]
+        return dp[n-1][m-1]
+        
+        
