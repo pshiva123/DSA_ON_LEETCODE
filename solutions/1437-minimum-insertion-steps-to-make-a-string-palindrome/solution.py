77b401a31f8f796eb97f1a8fc2371f914555b1e6
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2025-06-22 03:17:04 +0000

    Sync LeetCode submission Runtime - 643 ms (16.36%), Memory - 19.4 MB (88.21%)

---
+++ solutions/1437-minimum-insertion-steps-to-make-a-string-palindrome/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        s1=s
+        s2=s[::-1]
+        n=len(s)
+        dp=[[0]*(n+1) for i in range(n+1)]
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                if s1[i-1]==s2[j-1]:
+                    dp[i][j]=1+dp[i-1][j-1]
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        return n-dp[-1][-1]
