81b76c0a312cc68db90c85978c428fa33953e55c
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2025-08-24 17:42:11 +0000

    Sync LeetCode submission Runtime - 2 ms (37.76%), Memory - 17.9 MB (100.00%)

--- solutions/0020-valid-parentheses/solution.py
+++ solutions/0020-valid-parentheses/solution.py
@@ -1,38 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         li=[]
-        if s[0]=="]" or s[0]=="}" or s[0]=="]":
-            return False
-        li.append(s[0])
-        for i in range(1,len(s)):
-            if s[i]=="{" or s[i]=="(" or s[i]=="[":
-                li.append(s[i])
-            elif s[i]=="}":
-                if(len(li)==0):
-                    return False
-                if(li[-1]=="{"):
+        ss=set('{[(')
+        for ch in s:
+            if ch in ss:
+                li.append(ch)
+            else:
+                if len(li)==0:
+                    print(11)
+                if (ch=='}' and li and li[-1]=='{') or(
+                    ch==']' and li and li[-1]=='[') or(
+                    ch==')' and li and li[-1]=='('):
                     li.pop()
                 else:
-                    li.append(s[i])
-            elif s[i]=="]":
-                if(len(li)==0):
                     return False
-                if(li[-1]=="["):
-                    li.pop()
-                else:
-                    li.append(s[i])
-            elif s[i]==")":
-                if(len(li)==0):
-                    return False
-                if(li[-1]=="("):
-                    li.pop()
-                else:
-                    li.append(s[i])
-        if(len(li)==0):
-            return True
-        return False
-            
-            
-
-           
+        return len(li)==0
         
