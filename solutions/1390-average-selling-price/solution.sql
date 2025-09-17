1fd27d31ee6a27ced93bb8df28b01ae46452ed3d
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2025-09-17 04:22:46 +0000

    Sync LeetCode submission Runtime - 755 ms (94.19%), Memory - 0.0B (100.00%)

---
+++ solutions/1390-average-selling-price/solution.sql
@@ -0,0 +1,13 @@
+SELECT
+  p.product_id,
+  COALESCE(
+    ROUND( SUM(p.price * u.units) / NULLIF(SUM(u.units), 0), 2 ),
+    0
+  ) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+  ON p.product_id = u.product_id
+  AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id
+ORDER BY p.product_id;
+# here collesce is used to assign 0 and nullif to handle division by zero
