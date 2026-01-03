481b058acb25958f269270541aa75e6afcb36de9
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2026-01-03 08:14:29 +0000

    Sync LeetCode submission Runtime - 11 ms (66.86%), Memory - 19.3 MB (99.99%)

--- solutions/0645-set-mismatch/solution.py
+++ solutions/0645-set-mismatch/solution.py
@@ -1,28 +1,21 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        check=set(nums)
-        temp=[i for i in range(1,len(nums)+1)]
-        nums.extend(temp)
-        xor=0
+        s=set()
+        rep=-1
+        n=len(nums)
         for i in nums:
-            xor^=i
-        diff=xor  & -xor
-        x=0
-        y=0
-        for i in nums:
-            if i&diff:
-                x^=i
-            else:
-                y^=i
-        li=[]
-        if x in check:
-            li.append(x)
-            li.append(y)
-        else:
-            li.append(y)
-            li.append(x)
-        return li
-       
+            if i in s:
+                rep=i
+                break
+            s.add(i)
+        s=set(nums)
+        for i in range(1,n+1):
+            if i not in s:
+                return [rep,i]
         
+        
+        
+        
+
 
         
