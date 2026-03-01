f8dd9d89b54d9626585d702249c9d39e0dd2fd18
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2026-03-01 12:36:00 +0000

    Sync LeetCode submission Runtime - 15 ms (85.22%), Memory - 20 MB (39.17%)

---
+++ solutions/1807-partitioning-into-minimum-number-of-deci-binary-numbers/solution.py
@@ -0,0 +1,30 @@
+
+from collections import deque
+
+class Solution:
+    # li=[]
+
+    # q=deque(["1"])
+
+    # while q:
+    #     s=q.popleft()
+    #     if len(s)>10**5:
+    #         continue
+    #     num=int(s)
+
+
+    #     li.append(num)
+
+    #     q.append(s+"0")
+    #     q.append(s+"1")
+    # li.sort()
+
+
+    def minPartitions(self, s: str) -> int:
+        temp=max(s)
+        return int(temp)
+
+        
+        
+        
+        
