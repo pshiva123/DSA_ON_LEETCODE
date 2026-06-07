9b56f4d3183a1c880c33c9cb5ee85f44e04c150e
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2026-06-07 02:34:13 +0000

    Sync LeetCode submission Runtime - 7 ms (18.13%), Memory - 19.2 MB (92.42%)

---
+++ solutions/4319-sum-of-compatible-numbers-in-range-i/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sumOfGoodIntegers(self, n: int, k: int) -> int:
+        s=0
+        for x in range(1,n+k+101):
+            if abs(n-x)<=k and (n&x)==0:
+                s+=x
+        return s
+        
