8ddd9d1d511767a63342ce76e2202791402e9cdf
pshiva123 <154683099+pshiva123@users.noreply.github.com>
2026-06-11 11:54:45 +0000

    Sync LeetCode submission Runtime - 1 ms (57.18%), Memory - 19.4 MB (19.25%)

---
+++ solutions/0350-intersection-of-two-arrays-ii/solution.py
@@ -0,0 +1,13 @@
+from collections import Counter
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        count=Counter(nums1)
+        ans=[]
+        for i in nums2:
+            if i in count and count[i]>0:
+                count[i]-=1
+                ans.append(i)
+        return ans
+
+
+        
